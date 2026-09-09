5f2cf6c98326762998b0e4cf033678f4cb5618ab
Tiavina <randrianjatiavinaeliot@gmail.com>
2026-09-09 15:26:08 +0300

    Contact Page components implemented

--- src/components/ui/ContactForm.jsx
+++ src/components/ui/ContactForm.jsx
@@ -18,18 +18,20 @@ const ContactForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className='relative flex flex-1 flex-col gap-4 w-full max-w-xl mt-10'>
+    <form onSubmit={handleSubmit} className=' flex flex-1 flex-col gap-4 w-full max-w-xl mt-10'>
       <InputField Label="Nom complet" name="name" placeholder="Nom et prénom" value={form.name} onChange={handleChange} required />
       <InputField Label="Email" Type="email" name="email" placeholder="email@gmail.com" value={form.email} onChange={handleChange} required />
       <InputField Label="Numéro téléphone" Type="tel" name="phone" placeholder="+261340000000" value={form.phone} onChange={handleChange} />
       <TextAreaField Label="Message" name="message" placeholder="Votre message..." value={form.message} onChange={handleChange} required />
-      <button
-        type="submit"
-        className=" bg-jci-yellow text-jci-white hover:text-jci-yellow justify-center px-5 py-2 rounded-[10px] font-semi-bold text-[13px] w-fit hover:bg-transparent border-jci-yellow border-[0.15px] transition flex flex-row imets-center gap-2"
-      >
-        <RiSendInsLine size={20} />
-        Envoyer 
-      </button>
+      <div className='flex justify-end '>
+        <button
+          type="submit"
+          className=" bg-jci-yellow text-jci-white hover:text-jci-yellow justify-center px-5 py-2 rounded-[10px] font-semi-bold text-[13px] w-fit hover:bg-transparent border-jci-yellow border-[0.15px] transition flex flex-row imets-center gap-2"
+        >
+          <RiSendInsLine size={20} />
+          Envoyer 
+        </button>
+      </div>
     </form>
   )
 }
--- src/components/ui/ContactInfoBlock.jsx
+++ src/components/ui/ContactInfoBlock.jsx
@@ -5,16 +5,16 @@ import JCILogo from "../../images/JCI/JCI Madagascar/JCI_Madagascar_background_m
 
 const ContactInfoBlock = ({ Title, Address, Phone, Email }) => {
   return (
-    <div className='relative flex flex-1 flex-col gap-2 text-[13px] px-5 pb-5 pt-15 text-jci-black/80 bg-jci-blue rounded-xl justify-between overflow-hidden'>
+    <div className=' flex flex-1 flex-col gap-2 text-[13px] px-5 pb-5 pt-15 text-jci-black/80 bg-jci-blue rounded-xl justify-between overflow-hidden'>
       {Title && <h3 className='font-poppins font-medium text-jci-white text-[20px] uppercase'>{Title}</h3>}
-      <div className='flex flex-col gap-1 w-full h-full bg-jci-navy/80 rounded p-2'>
+      <div className=' relative flex flex-col gap-1 w-full h-full bg-jci-navy/80 rounded p-2'>
         <div className="w-full flex flex-col gap-3 h-full bg-jci-dark px-5 py-10 text-jci-white font-normal text-[12px]">
           {Address && <p className="flex flex-row items-center gap-2"><span className="text-jci-yellow"><FaMapMarkerAlt size={20} /></span>{Address}</p>}
           {Phone && <p className="flex flex-row items-center gap-2"><span className="text-jci-yellow"><BsFillTelephoneFill size={20} /></span>{Phone}</p>}
           {Email && <p className="flex flex-row items-center gap-2"><span className="text-jci-yellow"><IoMail size={20} /></span>{Email}</p>}
         </div>
-        <div className='absolute -bottom-6 right-0'>
-          <img src={JCILogo} alt="JCI Madagascar Logo" className="h-50 w-auto opacity-20" />
+        <div className='absolute -bottom-6 right-0 overflow-hidden'>
+          <img src={JCILogo} alt="JCI Madagascar Logo" className="md:h-50 h-20 w-auto opacity-20" />
         </div>
       </div>
     </div>
